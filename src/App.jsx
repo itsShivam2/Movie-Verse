98a5d00141c342d993abbf898436eb2ce7a107e4
itsShivam2 <135848325+itsShivam2@users.noreply.github.com>
2024-06-16 20:41:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-400 h-screen">
+        <h1 className="text-2xl text-center font-semibold bg-red-600 p-2">
+          MovieVerse
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
