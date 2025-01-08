fcad693fa6237c35f2f4f007c39c06379a1e9a41
itsShivam2 <shivamgupta7376@gmail.com>
2025-01-08 10:39:10 +0530

    bug fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-gray-900 border-t border-gray-800 text-gray-400 text-sm">
       <div className="container mx-auto px-4 py-6 text-center">
-        <p>© 2025 MovieApp. All rights reserved.</p>
+        <p>© 2024 MovieApp. All rights reserved.</p>
         <p>
           Made with <span className="text-red-500">❤</span> by Shivam Gupta
         </p>
